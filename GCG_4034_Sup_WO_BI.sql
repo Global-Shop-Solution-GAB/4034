f76b03861669c0775f706f47d7110c87d7ba8b19
Service Web <sw@gssmail.com>
2024-03-11 11:35:33 -0500

    Commit changes in C:\Websites\Git\emarlow\4034\GCG_4034_Sup_WO_BI.sql

---
+++ GCG_4034_Sup_WO_BI.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "GCG_4034_Sup_WO_BI"(
+ "Job" CHAR(6),
+ "Suffix" CHAR(3),
+ "Part" CHAR(20),
+ "Qty_Order" NUMERIC(12,4),
+ "Qty_Complete" NUMERIC(12,4),
+ "Type" CHAR(1),
+ "Dollar_Est" NUMERIC(10,2),
+ "Dollar_Act" NUMERIC(10,2),
+ "Hour_Est" NUMERIC(12,4),
+ "Hour_Act" NUMERIC(12,4),
+ "Date" CHAR(12),
+ "BIRunID" BIGINT,
+ "Description" CHAR(50));
